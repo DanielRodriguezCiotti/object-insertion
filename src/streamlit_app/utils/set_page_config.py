e238bc48e8595c36839afff48df79016040eef3d
DanielRodriguezCiotti <darc2203@gmail.com>
2024-10-04 13:27:36 +0000

    cleaning streamlit

--- src/streamlit_app/utils/set_page_config.py
+++
@@ -1,9 +0,0 @@
-"""Util to set common page config to all streamlit pages."""
-import streamlit as st
-
-STREAMLIT_TITLE = "object-insertion"
-
-
-def set_page_config() -> None:
-    """Set page config with wide layout and page title."""
-    st.set_page_config(layout="wide", page_title=STREAMLIT_TITLE)

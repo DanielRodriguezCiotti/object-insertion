9ea6d0ea9f3652dd53b09d7b9bf2c14e22be59ab
Daniel Rodriguez Ciotti <daniel.rodriguezciotti@sicara.com>
2024-10-21 12:37:12 +0200

    feat: final scripts version

--- src/scripts/compute_lpips.py
+++ src/scripts/compute_lpips.py
@@ -7,50 +7,102 @@ import lpips
 import torch
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-d0", "--dir0", type=str, default="./imgs/ex_dir0")
-parser.add_argument("-d1", "--dir1", type=str, default="./imgs/ex_dir1")
-parser.add_argument("-o", "--out", type=str, default="./imgs/example_dists.txt")
-parser.add_argument("-v", "--version", type=str, default="0.1")
-parser.add_argument("--use_gpu", action="store_true", help="turn on flag to use GPU")
-
-args = parser.parse_args()
-
-## Initializing the model
-loss_fn = lpips.LPIPS(net="alex", version=args.version)
-if args.use_gpu:
-    loss_fn.cuda()
-
-# crawl directories
-f = open(args.out.replace(".txt", "_all.txt"), "w")  # noqa: SIM115
-files = os.listdir(args.dir0)
-
-distances = []
-for file in tqdm(files):
-    if os.path.exists(os.path.join(args.dir1, file)):
-        # Load images
-        img0 = lpips.im2tensor(
-            lpips.load_image(os.path.join(args.dir0, file))
-        )  # RGB image from [-1,1]
-        img1 = lpips.im2tensor(lpips.load_image(os.path.join(args.dir1, file)))
-
-        if args.use_gpu:
-            img0 = img0.cuda()
-            img1 = img1.cuda()
-
-        # Compute distance
-        with torch.no_grad():
-            dist01 = loss_fn.forward(img0, img1)
-
-        distances.append(dist01.squeeze().item())  # type: ignore
-        f.writelines(f"{file}: {dist01.squeeze().item():.6f}\n")  # type: ignore
+from src.utils import resize_images  # Assuming you want to use resize logic if needed
 
 
-f.close()
+def compute_lpips(
+    output_file: str,
+    ground_truth_dir: str,
+    generated_dir: str,
+    version: str,
+) -> None:
+    """Compute the LPIPS metric between images in two directories."""
+    # Initialize the LPIPS model
+    loss_fn = lpips.LPIPS(net="alex", version=version)
+    if torch.cuda.is_available():
+        loss_fn.cuda()
 
-f_agg = open(args.out, "w")  # noqa: SIM115
-f_agg.writelines("Aggregated results:\n")
-f_agg.writelines("Mean: %.6f" % (sum(distances) / len(distances)))
-f_agg.writelines("Max: %.6f" % max(distances))
-f_agg.writelines("Min: %.6f" % min(distances))
-f_agg.close()
+    gt_files = os.listdir(ground_truth_dir)
+    distances = []
+
+    # Open file to log individual LPIPS scores
+    with open(output_file.replace(".txt", "_all.txt"), "w") as f:
+        for file in tqdm(gt_files):
+            gt_file_path = os.path.join(ground_truth_dir, file)
+            gen_file_path = os.path.join(generated_dir, file)
+
+            if os.path.exists(gen_file_path):
+                # Load the images
+                images = {}
+                images["gt"] = lpips.load_image(gt_file_path)
+                images["gen"] = lpips.load_image(gen_file_path)
+                if images["gt"].shape != images["gen"].shape:
+                    images = resize_images(images)
+                # Convert the images to tensors
+                img0 = lpips.im2tensor(images["gt"])
+                img1 = lpips.im2tensor(images["gen"])
+
+                # Move to GPU if required
+                if torch.cuda.is_available():
+                    img0 = img0.cuda()
+                    img1 = img1.cuda()
+
+                # Compute LPIPS between the two images
+                with torch.no_grad():
+                    lpips_value = loss_fn.forward(img0, img1).squeeze().item()
+
+                # Write the LPIPS value to the file
+                f.write(f"{file}: LPIPS = {lpips_value:.6f}\n")
+                distances.append(lpips_value)
+
+    # Aggregate the results and write to another file
+    with open(output_file, "w") as f_agg:
+        f_agg.writelines("Aggregated results:\n")
+        f_agg.writelines(f"Mean: {sum(distances) / len(distances):.6f}\n")
+        f_agg.writelines(f"Max: {max(distances):.6f}\n")
+        f_agg.writelines(f"Min: {min(distances):.6f}\n")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-d0",
+        "--dir0",
+        type=str,
+        required=True,
+        help="Path to the ground truth image directory",
+    )
+    parser.add_argument(
+        "-d1",
+        "--dir1",
+        type=str,
+        required=True,
+        help="Path to the generated image directory",
+    )
+    parser.add_argument(
+        "-o",
+        "--out",
+        type=str,
+        required=True,
+        help="Output file to store LPIPS results",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        type=str,
+        default="0.1",
+        help="Version of the LPIPS model to use",
+    )
+    parser.add_argument(
+        "--use_gpu", action="store_true", help="Turn on flag to use GPU"
+    )
+
+    args = parser.parse_args()
+
+    compute_lpips(args.out, args.dir0, args.dir1, args.version)
+
+
+if __name__ == "__main__":
+    main()
--- src/scripts/compute_ssim.py
+++ src/scripts/compute_ssim.py
@@ -8,43 +8,66 @@ from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument(
-    "-d0", "--dir0", type=str, required=True, help="Path to the first image directory"
-)
-parser.add_argument(
-    "-d1", "--dir1", type=str, required=True, help="Path to the second image directory"
-)
-parser.add_argument(
-    "-o", "--out", type=str, required=True, help="Output file to store SSIM results"
-)
+from src.utils import resize_images
 
-args = parser.parse_args()
 
-# Open the output file for writing results
-with open(args.out.replace(".txt", "_all.txt"), "w") as f:
-    files = os.listdir(args.dir0)
-
+def compute_ssim(output_file: str, ground_truth_dir: str, generated_dir: str):
+    """Compute the SSIM metric between images in two directories."""
+    gt_files = os.listdir(ground_truth_dir)
     distances = []
-    for file in tqdm(files):
-        file_path1 = os.path.join(args.dir0, file)
-        file_path2 = os.path.join(args.dir1, file)
+    with open(output_file.replace(".txt", "_all.txt"), "w") as f:
+        for file in tqdm(gt_files):
+            gt_file_path = os.path.join(ground_truth_dir, file)
+            gen_file_path = os.path.join(generated_dir, file)
 
-        if os.path.exists(file_path2):
-            # Load the images
-            image1 = np.array(Image.open(file_path1).convert("L"))
-            image2 = np.array(Image.open(file_path2).convert("L"))
+            if os.path.exists(gen_file_path):
+                # Load the images
+                images = {}
+                images["gt"] = np.array(Image.open(gt_file_path).convert("L"))
+                images["gen"] = np.array(Image.open(gen_file_path).convert("L"))
+                # Resize the images to the common size if needed
+                if images["gt"].shape != images["gen"].shape:
+                    images = resize_images(images)
+                # Compute SSIM between the two images
+                ssim_value = ssim(images["gt"], images["gen"])
+                # Write the SSIM value to the file
+                f.write(f"{file}: SSIM = {ssim_value:.6f}\n")
+                distances.append(ssim_value)
 
-            # Compute SSIM between the two images
-            ssim_value = ssim(image1, image2)
+    # Aggregate the results and write to another file
+    with open(output_file, "w") as f_agg:
+        f_agg.writelines("Aggregated results:\n")
+        f_agg.writelines(f"Mean: {sum(distances) / len(distances):.6f}\n")
+        f_agg.writelines(f"Max: {max(distances):.6f}\n")
+        f_agg.writelines(f"Min: {min(distances):.6f}\n")
 
-            # Write the SSIM value to the file
-            f.write(f"{file}: SSIM = {ssim_value:.6f}\n")
-            distances.append(ssim_value)
 
-# Aggregate the results and write to another file
-with open(args.out, "w") as f_agg:
-    f_agg.writelines("Aggregated results:\n")
-    f_agg.writelines(f"Mean: {sum(distances) / len(distances):.6f}\n")
-    f_agg.writelines(f"Max: {max(distances):.6f}\n")
-    f_agg.writelines(f"Min: {min(distances):.6f}\n")
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-d0",
+        "--dir0",
+        type=str,
+        required=True,
+        help="Path to the ground truth image directory",
+    )
+    parser.add_argument(
+        "-d1",
+        "--dir1",
+        type=str,
+        required=True,
+        help="Path to the generated image directory",
+    )
+    parser.add_argument(
+        "-o", "--out", type=str, required=True, help="Output file to store SSIM results"
+    )
+
+    args = parser.parse_args()
+
+    compute_ssim(args.out, args.dir0, args.dir1)
+
+
+if __name__ == "__main__":
+    main()
